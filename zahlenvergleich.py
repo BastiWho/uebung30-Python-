2cbc2219885ef1e56247a8b4e81ec549d257900c
Basti <sebastian.hufeld@docc.techstarter.de>
2023-03-22 19:40:45 +0100

    Zahlenvergleich

---
+++ zahlenvergleich.py
@@ -0,0 +1,15 @@
+print("\nZahlenvergleich")
+print("===============\n")
+
+a = int(input("Gib bitte deine erste Zahl ein: "))
+b = int(input("Gib bitte deine zweite Zahl ein: "))
+c = int(input("Gib nun bitte deine dritte Zahl ein: "))
+
+max_num = max(a, b, c)
+min_num = min(a, b, c)
+
+print("\nDie kleinste Zahl, die eingegeben wurde, ist:", min_num)
+print("Die größte eingegebene Zahl ist:", max_num)
+
+print("\nDanke dass du dieses Programm verwendet hast :) \n")
+print("=================================================")
